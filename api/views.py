df9c87e82c4587dd62003524d90717730a31b72d
code043 <procoder043@gmail.com>
2024-11-30 00:17:53 -0300

    Views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .serializer import UserSerializer
 
-# Create your views here.
+# GET all users
+@api_view(['GET'])
+def get_users(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+# POST a new user
+@api_view(['POST'])
+def create_user(request): 
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# GET a single user by ID
+@api_view(['GET'])
+def get_user(request, pk):
+    try:    
+        user = User.objects.get(pk=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+    except User.DoesNotExist:
+        return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+
+# PUT (update) a user by ID
+@api_view(['PUT'])
+def update_user(request, pk):
+    try:    
+        user = User.objects.get(pk=pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except User.DoesNotExist:
+        return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+
+# DELETE a user by ID
+@api_view(['DELETE'])
+def delete_user(request, pk):
+    try:    
+        user = User.objects.get(pk=pk)
+        user.delete()
+        return Response({"message": "User deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+    except User.DoesNotExist:
+        return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
